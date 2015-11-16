eb68051a3e851107a0dfae54318ce090e7773ff7
Pierre Le Gall <pierre@legall.im>
2015-11-16 21:52:57 +0100

    [Emacs] Switch to Gnu and Melpa package providers only.

--- emacs/init.el
+++ emacs/init.el
@@ -145,9 +145,7 @@
 
 ;;;; Packages
 
-(setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa"     . "http://melpa.milkbox.net/packages/")))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
 ;;;; Theme and faces
 
