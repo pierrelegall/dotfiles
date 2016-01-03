85abb4d27a5c5b3642d73aa671f48c02b283fbb4
Pierre Le Gall <pierre@legall.im>
2016-01-03 12:28:20 +0100

    [Emacs] Org configuration updated

--- emacs/init.el
+++ emacs/init.el
@@ -141,9 +141,20 @@
 
 ;;;; Org
 
-(setq org-todo-keywords '((sequence "TODO" "IN PROGRESS" "|" "DONE")))
-(setq org-agenda-files '("~/Notes/Organizer/"))
-(setq org-hide-leading-stars t)
+(let ((map my-global-map))
+  (define-key map (kbd "<M-up>") 'org-move-subtree-up)
+  (define-key map (kbd "<M-down>") 'org-move-subtree-down))
+
+(setq org-agenda-files '("~/Documents/Organizer/"))
+(setq org-todo-keywords '((sequence "TODO(t)" "|" "DONE(d)")))
+(setq org-agenda-todo-ignore-scheduled t)
+(setq org-agenda-todo-list-sublevels nil)
+
+(setq org-latex-pdf-process
+      '("pdflatex -interaction nonstopmode -output-directory %o %f"
+        "bibtex $(basename %b)"
+        "pdflatex -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -interaction nonstopmode -output-directory %o %f"))
 
 ;;;; Ido
 
