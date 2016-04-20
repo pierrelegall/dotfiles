99bb07cfe969cc5760fcf7b9b2ee23486e96b622
Pierre Le Gall <pierre@legall.im>
2016-04-20 23:24:23 +0200

    [Emacs] Eshell prompt changed

--- emacs/init.el
+++ emacs/init.el
@@ -159,6 +159,24 @@
 ;;;; Eshell
 
 (setq eshell-cmpl-ignore-case t)
+(setq eshell-banner-message ";; Welcome to the Emacs shell\n")
+(setq eshell-prompt-regexp "^\\[[^@]*[^\\[]*\\]\\n[#$] ")
+(setq eshell-prompt-function
+  (lambda ()
+    (concat
+     (propertize "\n")
+     (propertize "[" 'face `(:foreground "dodger blue" :weight bold))
+     (propertize (user-login-name) 'face `(:foreground "#d23681" :weight bold))
+     (propertize "@" 'face `(:foreground "dodger blue" :weight bold))
+     (propertize (system-name) 'face `(:foreground "#d23681" :weight bold))
+     (propertize " ")
+     (propertize (eshell/pwd) 'face `(:foreground "#2aa097" :weight bold))
+     (propertize "]" 'face `(:foreground "dodger blue" :weight bold))
+     (propertize "\n")
+     (if (= (user-uid) 0)
+         (propertize "#" 'face `(:foreground "orange red"))
+       (propertize "$" 'face `(:foreground "medium sea green" :weight bold)))
+     (propertize " "))))
 
 ;;;; Org
 
