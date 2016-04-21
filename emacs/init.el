aab5f7f3ce2f40b809f9e4a299b94d6c4be8faaf
Pierre Le Gall <pierre@legall.im>
2016-04-21 21:30:29 +0200

    [Emacs] eshell-prompt-regexp updated

--- emacs/init.el
+++ emacs/init.el
@@ -160,7 +160,7 @@
 
 (setq eshell-cmpl-ignore-case t)
 (setq eshell-banner-message ";; Welcome to the Emacs shell\n")
-(setq eshell-prompt-regexp "^\\[[^@]*[^\\[]*\\]\\n[#$] ")
+(setq eshell-prompt-regexp "[#$] ")
 (setq eshell-prompt-function
   (lambda ()
     (concat
