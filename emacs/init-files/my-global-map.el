0904097d12ed18e8996a0786c075ab340907fd9b
Pierre Le Gall <pierre@legall.im>
2016-04-24 12:11:19 +0200

    [Emacs] My global map externalized

---
+++ emacs/init-files/my-global-map.el
@@ -0,0 +1,76 @@
+;; * My global map
+
+;; ** Initialization
+
+(defvar my-global-map (make-keymap) "my global map")
+(define-minor-mode my-global-map-mode "my global map" :keymap my-global-map)
+(setq-default my-global-map-mode 1)
+
+;; ** Definition
+
+(let ((map my-global-map))
+  (define-key map (kbd "C-?") 'help-command)
+  (define-key map (kbd "C-x C-h") 'help-command)
+
+  (define-key map (kbd "C-w") 'kill-region-or-backward-word)
+  (define-key map (kbd "C-S-w") 'kill-ring-save)
+  (define-key map (kbd "C-S-y") 'yank-pop)
+  (define-key map (kbd "C-x C-k") 'kill-this-buffer)
+  (define-key map (kbd "C-c C-k") 'kill-this-buffer-and-delete-file)
+  (define-key map (kbd "C-%") 'query-replace)
+
+  (define-key map (kbd "C-h") (kbd "<backspace>"))
+  (define-key map (kbd "C-S-h") (kbd "C-<backspace>"))
+  (define-key map (kbd "C-S-d") 'kill-word)
+
+  (define-key map (kbd "C-v") 'my-scroll-up)
+  (define-key map (kbd "M-v") 'my-scroll-down)
+  (define-key map (kbd "C-S-v") 'my-scroll-down)
+
+  (define-key map (kbd "C-;") 'execute-extended-command)
+  (define-key map (kbd "C-:") 'shell-command)
+  (define-key map (kbd "C-!") 'eshell-command)
+  (define-key map (kbd "C-`") 'rename-buffer)
+  (define-key map (kbd "C-c C-t") 'multi-term)
+  (define-key map (kbd "C-c t") 'multi-term-next)
+
+  (define-key map (kbd "C-'") 'select-by-step)
+  (define-key map (kbd "C-j") 'toggle-letter-case)
+
+  (define-key map (kbd "C-,") 'backward-word)
+  (define-key map (kbd "C-.") 'forward-word)
+  (define-key map (kbd "C-S-b") 'backward-word)
+  (define-key map (kbd "C-S-f") 'forward-word)
+  (define-key map (kbd "C-<") 'beginning-of-buffer)
+  (define-key map (kbd "C->") 'end-of-buffer)
+
+  (define-key map (kbd "<C-tab>") 'next-buffer)
+  (define-key map (kbd "<C-iso-lefttab>") 'previous-buffer)
+
+  (define-key map (kbd "C-S-l") 'goto-line)
+
+  (define-key map (kbd "C-;") 'execute-extended-command)
+  (define-key map (kbd "C-#") 'comment-or-uncomment-line-or-region)
+
+  (define-key map (kbd "C-/") 'undo)
+  (define-key map (kbd "C-\\") 'redo)
+  (define-key map (kbd "C-S-r") 'revert-buffer)
+
+  (define-key map (kbd "C-o")
+    (lambda() (interactive) (other-window 1)))
+  (define-key map (kbd "C-S-o")
+    (lambda() (interactive) (other-window -1)))
+  (define-key map (kbd "C-x o")
+    (lambda() (interactive) (other-window 1)))
+  (define-key map (kbd "C-x O")
+    (lambda() (interactive) (other-window -1)))
+
+  (define-key map (kbd "C-S-p") 'move-line-or-region-above)
+  (define-key map (kbd "C-S-n") 'move-line-or-region-below)
+
+  (define-key map (kbd "C-c a") 'org-agenda)
+  (define-key map (kbd "C-c l") 'org-store-link)
+
+  (define-key map (kbd "C-c s") 'magit-status)
+
+  (define-key map [C-f1] 'show-absolute-buffer-file-path))
