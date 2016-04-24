d558cc6dbff50ae98a60118dc74dd13cca07ddca
Pierre Le Gall <pierre@legall.im>
2016-04-24 13:41:08 +0200

    [Emacs] Cleaning init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,31 @@
 ;; * Emacs configuration entry point
 
+;; ** Package initialization
+
+(defvar my/package-initialize-file
+  "~/.config/emacs/package-initialize.el"
+  "Package initialization file.")
+
+(defun my/package-initialize ()
+  "Load my/package-initialize-file."
+  (load-file my/package-initialize-file))
+
+(my/package-initialize)
+
+;; ** Functions initialization
+
+(defvar my/functions-file
+  "~/.config/emacs/functions.el"
+  "My functions file.")
+
+(defun my/load-functions ()
+  "Load my functions file."
+  (load-file my/functions-file))
+
+(my/load-functions)
+
+;; ** Init files loading
+
 (defvar my/init-files-directory
   "~/.config/emacs/init-files"
   "Directory of the initialization files.")
@@ -14,25 +40,4 @@
             (load-file init-file))
           my/init-files))
 
-(defvar my/functions-file
-  "~/.config/emacs/functions.el"
-  "My functions file.")
-
-(defun my/load-functions ()
-  "Load my functions file."
-  (load-file my/functions-file))
-
-(defvar my/package-initialize-file
-  "~/.config/emacs/package-initialize.el"
-  "Package initialization file.")
-
-(defun my/package-initialize ()
-  "Load my/package-initialize-file."
-  (load-file my/package-initialize-file))
-
-(my/package-initialize)
-(my/load-functions)
 (my/load-init-files)
-
-(put 'dired-find-alternate-file 'disabled nil)
-
