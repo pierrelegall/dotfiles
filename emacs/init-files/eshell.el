c960cc5d74e04999315d4d141c21a9dc37ea5ceb
Pierre Le Gall <pierre@legall.im>
2016-04-24 12:22:31 +0200

    [Emacs] Eshell externalized

---
+++ emacs/init-files/eshell.el
@@ -0,0 +1,30 @@
+;; * Eshell
+
+;; ** Title
+
+(setq eshell-banner-message ";; Welcome to the Emacs shell\n")
+
+;; Completion
+
+(setq eshell-cmpl-ignore-case t)
+
+;; ** Prompt
+
+(setq eshell-prompt-function
+  (lambda ()
+    (concat
+     (propertize "\n")
+     (propertize "[" 'face `(:foreground "dodger blue" :weight bold))
+     (propertize (user-login-name) 'face `(:foreground "#d23681" :weight bold))
+     (propertize "@" 'face `(:foreground "dodger blue" :weight bold))
+     (propertize (system-name) 'face `(:foreground "#d23681" :weight bold))
+     (propertize " ")
+     (propertize (abbreviate-file-name (eshell/pwd)) 'face `(:foreground "#2aa097" :weight bold))
+     (propertize "]" 'face `(:foreground "dodger blue" :weight bold))
+     (propertize "\n")
+     (if (= (user-uid) 0)
+         (propertize "#" 'face `(:foreground "orange red"))
+       (propertize "$" 'face `(:foreground "medium sea green" :weight bold)))
+     (propertize " "))))
+
+(setq eshell-prompt-regexp "^[#$] ")
--- emacs/init.el
+++ emacs/init.el
@@ -17,28 +17,6 @@
 (package-initialize)
 (my/load-init-files)
 
-;;;; Eshell
-
-(setq eshell-cmpl-ignore-case t)
-(setq eshell-banner-message ";; Welcome to the Emacs shell\n")
-(setq eshell-prompt-regexp "[#$] ")
-(setq eshell-prompt-function
-  (lambda ()
-    (concat
-     (propertize "\n")
-     (propertize "[" 'face `(:foreground "dodger blue" :weight bold))
-     (propertize (user-login-name) 'face `(:foreground "#d23681" :weight bold))
-     (propertize "@" 'face `(:foreground "dodger blue" :weight bold))
-     (propertize (system-name) 'face `(:foreground "#d23681" :weight bold))
-     (propertize " ")
-     (propertize (abbreviate-file-name (eshell/pwd)) 'face `(:foreground "#2aa097" :weight bold))
-     (propertize "]" 'face `(:foreground "dodger blue" :weight bold))
-     (propertize "\n")
-     (if (= (user-uid) 0)
-         (propertize "#" 'face `(:foreground "orange red"))
-       (propertize "$" 'face `(:foreground "medium sea green" :weight bold)))
-     (propertize " "))))
-
 ;;;; Org
 
 (let ((map my-global-map))
