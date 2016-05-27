4ffd976b5a6b05f716159e715c967b51f3345a8a
Pierre Le Gall <pierre@legall.im>
2016-05-28 01:10:43 +0200

    [Emacs] web-mode config added

---
+++ emacs/init-files/web-mode.el
@@ -0,0 +1,15 @@
+;; * Web mode
+
+(require 'web-mode)
+
+;; ** Extensions
+
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+
+;; ** Indent
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-attr-indent-offset 2)
