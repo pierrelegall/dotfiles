7773a9f9e90c2df340e1bb15973acce9325c0ce5
Pierre Le Gall <pierre@legall.im>
2016-06-16 22:11:26 +0200

    [Emacs] Yascroll bar go to the right

--- emacs/init-files/yascroll.el
+++ emacs/init-files/yascroll.el
@@ -1,5 +1,5 @@
 ;; * Yascroll
 
-(setq yascroll:scroll-bar '(left-fringe right-fringe text-area))
+(setq yascroll:scroll-bar '(right-fringe))
 (setq yascroll:delay-to-hide 1)
 (global-yascroll-bar-mode t)
