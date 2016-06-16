3e6773f03418f27b77abb94f5d6f9461245c5185
Pierre Le Gall <pierre@legall.im>
2016-06-16 23:40:38 +0200

    [Emacs] Term mode config added

---
+++ emacs/init-files/term-mode.el
@@ -0,0 +1,20 @@
+(setq term-unbind-key-list
+  '("C-z" "C-x" "C-c" "C-y" "<ESC>"))
+
+(setq term-bind-key-alist
+  '(("C-c C-c" . term-interrupt-subjob)
+    ("C-g" . term-interrupt-subjob)
+    ("C-q" . term-send-raw)
+    ("M-f" . term-send-forward-word)
+    ("M-b" . term-send-backward-word)
+    ("C-h" . term-send-backspace)
+    ("C-p" . term-send-up)
+    ("C-n" . term-send-down)
+    ("C-y" . term-paste)
+    ("C-r" . term-send-reverse-search-history)
+    ("C-S-d" . term-send-forward-kill-word) ; x
+    ("C-S-h" . term-send-backward-kill-word) ; ~
+    ("C-S-f" . term-send-forward-word) ; x
+    ("C-S-b" . term-send-backward-word) ; x
+    ("C-S-s" . isearch-forward)
+    ("C-S-r" . isearch-backward)))
