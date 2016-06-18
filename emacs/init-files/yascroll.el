ec5f9f801da3aa15069b6f91354c022e3f0ecb6d
Pierre Le Gall <pierre@legall.im>
2016-06-18 17:58:15 +0200

    [Emacs] Yascroll bar back to the left

--- emacs/init-files/yascroll.el
+++ emacs/init-files/yascroll.el
@@ -1,5 +1,5 @@
 ;; * Yascroll
 
-(setq yascroll:scroll-bar '(right-fringe))
-(setq yascroll:delay-to-hide 1)
+(setq yascroll:scroll-bar '(left-fringe right-fringe))
+(setq yascroll:delay-to-hide 4)
 (global-yascroll-bar-mode t)
