5977e01da3937c3f28426296ba7f12111cb69dd7
Pierre Le Gall <pierre@legall.im>
2016-06-18 17:56:59 +0200

    [Emacs] Scroll step back to default

--- emacs/init-files/scrolling.el
+++ emacs/init-files/scrolling.el
@@ -1,7 +1,7 @@
 ;; Scrolling
 
 (setq scroll-margin 2)
-(setq scroll-step 4)
+(setq scroll-step 1)
 (setq mouse-wheel-follow-mouse (quote t))
 (setq mouse-wheel-mode t)
 (setq mouse-wheel-progressive-speed nil)
