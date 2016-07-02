ef061c2c8c05a340244e732d06fed956d24668a4
Pierre Le Gall <pierre@legall.im>
2016-07-02 15:33:47 +0200

    [Emacs] New ruby-mode config

--- emacs/init-files/ruby-mode.el
+++ emacs/init-files/ruby-mode.el
@@ -1,9 +1,29 @@
 ;; * Ruby mode
 
-(add-hook 'ruby-mode-hook 'robe-mode)
+(require 'ruby-mode)
 
 (set 'ruby-insert-encoding-magic-comment nil)
 
 ;; ** Robe
 
+(require 'robe)
+
+(add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'robe-mode-hook 'ac-robe-setup)
+
+(defun my/ruby-send-region-or-line ()
+  (interactive)
+  (let ((region (my/line-at-position-or-region)))
+    (ruby-send-region (car region) (car (cdr region)))))
+
+(define-key robe-mode-map (kbd "C-x C-e") 'my/ruby-send-region-or-line)
+
+;; ** Flymake
+
+(require 'flymake-ruby)
+
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+;; ** Company
+
+(eval-after-load 'company '(push 'company-robe company-backends))
