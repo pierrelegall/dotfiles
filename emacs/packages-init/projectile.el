397ee1c7dda2383132c71cb286b41783ba6f3aea
Pierre Le Gall <pierre@legall.im>
2016-07-11 21:39:06 +0200

    [Emacs] Projectile use helm now

--- emacs/packages-init/projectile.el
+++ emacs/packages-init/projectile.el
@@ -2,5 +2,13 @@
 
 (require 'projectile)
 (projectile-global-mode)
-;;(setq projectile-completion-system 'ido)
-;;(helm-projectile-on)
+
+;; ** Keybindings
+
+(let ((map projectile-mode-map))
+  (define-key map (kbd "C-c p g") 'helm-projectile-grep))
+
+;; ** Helm
+
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
