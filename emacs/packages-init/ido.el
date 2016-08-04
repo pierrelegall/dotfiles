95196a945fcc3d5777a0eb0f75d6916bde2b7896
Pierre Le Gall <pierre@legall.im>
2016-08-04 13:26:24 +0200

    [Emacs] Ido disabled

--- emacs/packages-init/ido.el
+++ emacs/packages-init/ido.el
@@ -1,13 +1,13 @@
 ;; * Ido
 
-(require 'ido)
+;; (require 'ido)
 
-(ido-mode t)
-(ido-everywhere t)
-(ido-vertical-mode t)
-(flx-ido-mode t)
+;; (ido-mode t)
+;; (ido-everywhere t)
+;; (ido-vertical-mode t)
+;; (flx-ido-mode t)
 
-(setq ido-vertical-define-keys 'C-n-C-p-C-s-C-r)
+;; (setq ido-vertical-define-keys 'C-n-C-p-C-s-C-r)
 
-(setq ido-ignore-buffers '("^ " "*Completions*" "*Shell Command Output*"
-                           "*Messages*" "Async Shell Command"))
+;; (setq ido-ignore-buffers '("^ " "*Completions*" "*Shell Command Output*"
+;;                            "*Messages*" "Async Shell Command"))
