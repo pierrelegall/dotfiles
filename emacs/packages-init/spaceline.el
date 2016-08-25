c816eb8e9371d1a836bf3baea78b65cefa4e7302
Pierre Le Gall <pierre@legall.im>
2016-08-25 19:01:51 +0200

    [Emacs] spaceline config added

---
+++ emacs/packages-init/spaceline.el
@@ -0,0 +1,18 @@
+;; * Spaceline
+
+(require 'spaceline-config)
+
+;; ** Theme
+
+(spaceline-emacs-theme)
+
+;; ** Customization
+
+(spaceline-toggle-buffer-size-off)
+(spaceline-toggle-minor-modes-off)
+(spaceline-toggle-line-column-off)
+(spaceline-toggle-buffer-encoding-abbrev-off)
+
+(spaceline-toggle-version-control-on)
+;;(spaceline-toggle-projectile-root-on) ; doesn't work
+;;(spaceline-toggle-flycheck-warning-on) ; doesn't work
