446f5e4c297a930142e3fcb09380000f37e73f64
Pierre Le Gall <pierre@legall.im>
2016-08-25 15:47:45 +0200

    [Emacs] spacemacs-dark theme customization add

---
+++ emacs/packages-init/.spacemacs-dark-theme.el
@@ -0,0 +1,10 @@
+;; * Spacemacs theme
+
+(setq spacemacs-theme-org-height nil)
+(setq spacemacs-theme-comment-bg nil)
+
+(custom-theme-set-faces
+ `spacemacs-dark
+ `(cursor         ((t (:foreground nil       :background "#dd2222"))))
+ `(lazy-highlight ((t (:foreground "#33cc33" :background nil))))
+ `(isearch        ((t (:foreground "#cc3333" :background nil)))))
