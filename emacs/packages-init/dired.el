840e806a246cf238c20f39385be536498444f670
Pierre Le Gall <pierre@legall.im>
2016-08-28 16:32:55 +0200

    [Emacs] Better Dired mode

--- emacs/packages-init/dired.el
+++ emacs/packages-init/dired.el
@@ -3,4 +3,14 @@
 (require 'dired)
 
 (setq dired-listing-switches "--group-directories-first -Alh --indicator-style=slash")
-(define-key dired-mode-map (kbd "C-m") 'dired-find-alternate-file)
+(put 'dired-find-alternate-file 'disabled nil)
+(setq dired-omit-files "^\\..*~?$")
+
+;; ** Maps
+
+(let ((map dired-mode-map))
+  (define-key map (kbd "h") 'dired-omit-mode))
+
+;; ** Hooks
+
+(add-hook 'dired-mode-hook 'dired-omit-mode)
