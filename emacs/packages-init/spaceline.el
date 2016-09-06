5cea5113dd15cbd5613a5be668b61699e576041f
Pierre Le Gall <pierre@legall.im>
2016-09-06 17:29:05 +0200

    [Emacs] Disable vc on the spaceline

--- emacs/packages-init/spaceline.el
+++ emacs/packages-init/spaceline.el
@@ -13,7 +13,7 @@
 (spaceline-toggle-line-column-off)
 (spaceline-toggle-buffer-encoding-abbrev-off)
 
-(spaceline-toggle-version-control-on)
+;;(spaceline-toggle-version-control-on)
 ;;(spaceline-toggle-projectile-root-on) ; doesn't work
 ;;(spaceline-toggle-flycheck-warning-on) ; doesn't work
 
