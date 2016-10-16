dea5e79efdc3d2ccf47a8fcfe113603eb8fa638e
Pierre Le Gall <pierre@legall.im>
2016-10-16 18:07:15 +0200

    [Emacs] Some auto-*things* enabled for web-mode

--- emacs/packages-init/web-mode.el
+++ emacs/packages-init/web-mode.el
@@ -20,6 +20,12 @@
 (setq web-mode-code-indent-offset 2)
 (setq web-mode-attr-indent-offset 2)
 
+;; ** Auto
+
+(setq web-mode-enable-auto-indentation t)
+(setq web-mode-enable-auto-closing t)
+(setq web-mode-enable-auto-pairing t)
+
 ;; ** Company
 
 (add-hook 'web-mode-hook (lambda ()
