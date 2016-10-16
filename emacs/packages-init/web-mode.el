ca831b89e5c53f48ce5ea9d03fc4d387682f6853
Pierre Le Gall <pierre@legall.im>
2016-10-16 18:06:54 +0200

    [Emacs] Use web-mode by default for HTML files

--- emacs/packages-init/web-mode.el
+++ emacs/packages-init/web-mode.el
@@ -9,6 +9,7 @@
 
 ;; ** Extensions
 
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 
