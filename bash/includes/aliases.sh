f789f6e9d931d38972b8efad6dde55c9adf2931f
Pierre Le Gall <pierre@legall.im>
2016-10-24 23:52:06 +0200

    [Bash] eshell-aliases-update alias patched

--- bash/includes/aliases.sh
+++ bash/includes/aliases.sh
@@ -41,5 +41,5 @@ alias qwfp='kb'
 
 alias eshell-aliases-update="\
   alias | \
-  sed -E \"s/^alias ([^=]+)='(.*)'$/alias \1 \2 \$*/g; s/'\\\''/'/g;\" \
+  sed -E \"s/^alias ([^=]+)='(.*)'$/alias \1 \2 \$\*/g; s/'\\\''/'/g;\" \
   >~/.emacs.d/eshell/alias"
