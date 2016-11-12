3738f08cd925b2714161fe9f36ef83e597c344cd
Pierre Le Gall <pierre@legall.im>
2016-11-12 18:44:14 +0100

    [Emacs] Robe config moved from ruby-mode.el to robe.el

--- emacs/packages-init/ruby-mode.el
+++ emacs/packages-init/ruby-mode.el
@@ -1,26 +1,5 @@
-;; * Ruby mode
-
-(my/ensure-package-installed 'robe)
+;; Ruby mode
 
 (require 'ruby-mode)
 
 (set 'ruby-insert-encoding-magic-comment nil)
-
-;; ** Robe
-
-(require 'robe)
-
-(add-hook 'ruby-mode-hook 'robe-mode)
-(eval-after-load 'company
-  '(push 'company-robe company-backends))
-
-(defun my/ruby-send-region-or-line ()
-  (interactive)
-  (let ((region (my/line-at-position-or-region)))
-    (ruby-send-region (car region) (car (cdr region)))))
-
-(define-key robe-mode-map (kbd "C-x C-e") 'my/ruby-send-region-or-line)
-
-;; ** Company
-
-(eval-after-load 'company '(push 'company-robe company-backends))
