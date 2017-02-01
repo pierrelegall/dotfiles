eda6e961b9aafcb7fb4577b85a8150af417ff089
Pierre Le Gall <pierre@legall.im>
2017-02-01 10:47:37 +0100

    [Emacs] Perfer magit-log-head instead of magit-log-all

--- emacs/packages-init/magit.el
+++ emacs/packages-init/magit.el
@@ -11,4 +11,5 @@
   (define-key map (kbd "C-c g f") 'magit-file-popup)
   (define-key map (kbd "C-c g a") 'magit-file-popup)
   (define-key map (kbd "C-c g d") 'magit-diff-buffer-file)
-  (define-key map (kbd "C-c g l") 'magit-log-all))
+  (define-key map (kbd "C-c g l") 'magit-log-head)
+  (define-key map (kbd "C-c g L") 'magit-log-all))
