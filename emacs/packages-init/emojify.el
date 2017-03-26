242ed2fbc8d9f8534f4ce4910205a33344099584
Pierre Le Gall <pierre@legall.im>
2017-03-26 22:29:07 +0200

    [Emacs] Add an emojify-insert-emoji binding

--- emacs/packages-init/emojify.el
+++ emacs/packages-init/emojify.el
@@ -5,4 +5,7 @@
 (setq emojify-display-style 'image)
 (setq emojify-emoji-styles '(github))
 
+(let ((map my/global-map))
+  (define-key map (kbd "C-S-j") 'emojify-insert-emoji))
+
 (global-emojify-mode)
