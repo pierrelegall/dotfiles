8bdf45618b6c5dda555f746a9cfee6bdffc3e165
Pierre Le Gall <pierre@legall.im>
2017-06-11 18:19:03 +0200

    [Emacs] Stop the isearch blinking

--- emacs/packages-init/isearch.el
+++ emacs/packages-init/isearch.el
@@ -1,6 +1,6 @@
-;; * isearch
+;; isearch
 
-;; ** Bindings
+(setq-default lazy-highlight-initial-delay 0)
 
 (define-key isearch-mode-map (kbd "<backspace>") 'isearch-del-char)
 (define-key isearch-mode-map (kbd "C-h") 'isearch-del-char)
