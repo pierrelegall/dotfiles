cc57ee87268912cc0e12b48bdcec142bdaae2f5d
Pierre Le Gall <pierre@legall.im>
2017-06-30 22:42:40 +0200

    [irb] Better helpers to find defined methods in objects

--- irb/irbrc.rb
+++ irb/irbrc.rb
@@ -1,12 +1,41 @@
 # BasicObject monkey patch
 class BasicObject
   # Returns the methods names (as symbol) defined
-  # in the object class but not in its superclass.
-  # Warning: it doesn't return redefined methods.
-  def defined_methods
+  # inside the object class.
+  def internal_methods
     return methods if self.instance_of? BasicObject
-    defined_methods = methods - self.class.superclass.instance_methods
-    return defined_methods
+    methods.select { |method_name| method(method_name).owner == self.class }
+  end
+
+  # Returns the methods names (as symbol) defined
+  # outside the object class.
+  # Warning: it doesn't return overrided methods.
+  def external_methods
+    (inherited_methods + extended_methods + included_methods).uniq
+  end
+
+  # Returns overrided methods.
+  def overrided_methods
+    internal_methods & external_methods
+  end
+
+  # Returns inherited methods.
+  def inherited_methods
+    self.class.superclass.instance_methods
+  end
+
+  # Returns extended methods.
+  def extended_methods
+    self.class.singleton_class.included_modules.reduce [] do |acc, wodule|
+      acc + wodule.instance_methods
+    end
+  end
+
+  # Returns included methods.
+  def included_methods
+    self.class.included_modules.reduce [] do |acc, wodule|
+      acc + wodule.instance_methods
+    end
   end
 end
 
