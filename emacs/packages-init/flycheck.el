424128147ff03a3460508dfd2b93b5c1f2f26a06
Pierre Le Gall <pierre@legall.im>
2017-07-16 18:03:33 +0200

    [Emacs] Reduce error check in emacs-lisp

--- emacs/packages-init/flycheck.el
+++ emacs/packages-init/flycheck.el
@@ -4,6 +4,9 @@
 
 (add-hook 'prog-mode-hook 'flycheck-mode)
 
+(setq-default flycheck-emacs-lisp-load-path 'inherit)
+(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+
 (setq flycheck-indication-mode nil)
 (setq flycheck-display-errors-delay 100) ; workaround to disable it
 
