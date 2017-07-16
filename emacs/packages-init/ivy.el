1d4faa5be50a43bd2e60317a39542199c8f9f75e
Pierre Le Gall <pierre@legall.im>
2017-07-16 20:02:33 +0200

    [Emacs] Use smex instead of an ad-hoc solution for ivy

--- emacs/packages-init/ivy.el
+++ emacs/packages-init/ivy.el
@@ -4,21 +4,11 @@
 (my/ensure-package-installed 'swiper)
 (my/ensure-package-installed 'counsel)
 (my/ensure-package-installed 'counsel-projectile)
+(my/ensure-package-installed 'smex)
 
 (ivy-mode +1)
 (counsel-mode +1)
 (counsel-projectile-on)
-
-(defun my/ivy--sort-by-length (_name candidates)
-  "Alphanumeric sort for ivy results."
-  (seq-sort 'string-lessp (seq-copy candidates)))
-
-(setq ivy-re-builders-alist
-      '((t . ivy--regex-ignore-order)))
-
-(setq ivy-sort-matches-functions-alist
-      '((ivy-switch-buffer . ivy-sort-function-buffer)
-        (t . my/ivy--sort-by-length)))
 
 (define-key isearch-mode-map (kbd "TAB") 'swiper-from-isearch)
 ;(define-key swiper-map (kbd "TAB") 'swiper-projectile-from-swiper)
