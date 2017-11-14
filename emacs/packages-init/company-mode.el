2d92a6b23027888b40ff19dd1197afe7d2bfa08f
Pierre Le Gall <pierre@legall.im>
2017-11-14 13:45:53 +0100

    [Emacs] Add a `company-show-doc-buffer` keybinding

--- emacs/packages-init/company-mode.el
+++ emacs/packages-init/company-mode.el
@@ -10,3 +10,4 @@
 
 (define-key company-active-map (kbd "C-n") #'company-select-next)
 (define-key company-active-map (kbd "C-p") #'company-select-previous)
+(define-key company-active-map (kbd "C-?") #'company-show-doc-buffer)
