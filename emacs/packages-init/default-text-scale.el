787d4939c1af0a27bdbeae4bdd55575ad3d76a77
Pierre Le Gall <pierre@legall.im>
2017-12-08 19:05:38 +0100

    [Emacs] Explicite font size for default-text-scale

--- emacs/packages-init/default-text-scale.el
+++ emacs/packages-init/default-text-scale.el
@@ -4,5 +4,7 @@
 
 (require 'default-text-scale)
 
+(setq default-text-scale-amount 12)
+
 (global-set-key (kbd "C-M--") 'default-text-scale-decrease)
 (global-set-key (kbd "C-M-=") 'default-text-scale-increase)
