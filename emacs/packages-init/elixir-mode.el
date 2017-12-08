04c6a986c96e3c33fcde1d649ab41451b8a784f9
Pierre Le Gall <pierre@legall.im>
2017-12-08 18:57:52 +0100

    [Emacs] Add missing `(require 'alchemist)`

--- emacs/packages-init/elixir-mode.el
+++ emacs/packages-init/elixir-mode.el
@@ -3,5 +3,7 @@
 (my/ensure-package-installed 'elixir-mode)
 (my/ensure-package-installed 'alchemist)
 
+(require 'alchemist)
+
 (add-to-list 'auto-mode-alist '("\\.ex\\'" . elixir-mode))
 (add-to-list 'auto-mode-alist '("\\.exs\\'" . elixir-mode))
