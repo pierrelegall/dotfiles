ae760595bfc05e059e8182a92a67fb384a603468
Pierre Le Gall <pierre@legall.im>
2018-10-21 18:39:58 +0200

    [Bash] Add the ~/.bin directory the PATH

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -6,6 +6,8 @@ source $HOME/.config/bash/includes/prompt.sh
 source $HOME/.config/bash/includes/plugins.sh
 source $HOME/.config/bash/includes/terminal-options.sh
 
+PATH=$PATH:~/.bin
+
 if [ -f ~/.bashrc-local ]; then
   source ~/.bashrc-local
 fi
