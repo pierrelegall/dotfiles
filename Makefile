01e7ec4145268174860075f70eb4a1a8796169a9
Pierre Le Gall <pierre@legall.im>
2021-12-30 02:53:02 +0100

    [*] Removed Makefiles superflu

--- Makefile
+++ Makefile
@@ -1,8 +1,4 @@
-# Variables
-
-SUB_DIRECTORIES = $(shell ls -d */)
-
-# Actions
+SUB_DIRS = $(shell ls -d */)
 
 all: print-info make-all
 
@@ -10,7 +6,7 @@ print-info:
 	@ echo "This makefile run all sub directories makefiles."
 
 make-all:
-	@ for each in $(SUB_DIRECTORIES); do \
+	@ for each in $(SUB_DIRS); do \
 		echo ""; \
 		make -C $$each --quiet; \
 	done
--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -1,10 +1,10 @@
 # Bashrc
 
-source $HOME/.config/bash/includes/exports.sh
-source $HOME/.config/bash/includes/aliases.sh
-source $HOME/.config/bash/includes/prompt.sh
-source $HOME/.config/bash/includes/plugins.sh
-source $HOME/.config/bash/includes/terminal-options.sh
+source $HOME/.shared-config/bash/includes/exports.sh
+source $HOME/.shared-config/bash/includes/aliases.sh
+source $HOME/.shared-config/bash/includes/prompt.sh
+source $HOME/.shared-config/bash/includes/plugins.sh
+source $HOME/.shared-config/bash/includes/terminal-options.sh
 
 PATH=$PATH:~/.bin
 
