93b5cb29347aa4b7456b63f70b8a77c7a99501c7
Pierre Le Gall <pierre@legall.im>
2024-01-05 14:58:26 +0100

    [Fish] add vterm support

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,13 @@
 set -U fish_greeting
 
-source $__fish_config_dir/prompt.fish
-source $HOME/.shared-config/bash/includes/aliases.sh
+source $HOME/.dotfiles/fish/prompt.fish
+source $HOME/.dotfiles/fish/emacs-vterm.fish
+source $HOME/.dotfiles/bash/includes/aliases.sh
+
+set ASDF_SOURCE /opt/asdf-vm/asdf.fish
+if test -f $ASDF_SOURCE
+  source $ASDF_SOURCE
+end
 
 bind \cg cancel
+bind --erase --all \cj
